90e09427d4d83fb19beb25b594c14a20700aa458
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-28 20:44:06 +0530

    Create Minimum Loss.java

---
+++ Problem 5/Minimum Loss.java
@@ -0,0 +1,24 @@
+Question Link 👇
+
+  https://www.hackerrank.com/contests/algohunt/challenges/minimum-loss/problem
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> price = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        int result = Result.minimumLoss(price);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
