4bec2949a05f13ebd5aba83809b741c268dd6d28
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-28 20:50:33 +0530

    Update Missing Numbers.java

--- Problem 3/Missing Numbers.java
+++ Problem 3/Missing Numbers.java
@@ -1,38 +1,10 @@
 
-Given two arrays of integers, find which elements in the second array are missing from the first array.
+Question Link 👇
 
-Example
-The  array is the orginal list. The numbers missing are .
+  https://www.hackerrank.com/contests/algohunt/challenges/missing-numbers
 
-Notes
-If a number occurs multiple times in the lists, you must ensure that the frequency of that 
-number in both lists is the same. If that is not the case, then it is also a missing number.
-Return the missing numbers sorted ascending.
-Only include a missing number once, even if it is missing multiple times.
-The difference between the maximum and minimum numbers in the original list is less than or equal to .
-Function Description
 
-Complete the missingNumbers function in the editor below. It should return a sorted array of missing numbers.
-
-missingNumbers has the following parameter(s):
-
-int arr[n]: the array with missing numbers
-int brr[m]: the original array of numbers
-
-Returns
-int[]: an array of integers
-
-Sample Input
-10
-203 204 205 206 207 208 203 204 205 206
-13
-203 204 204 205 206 207 205 208 203 206 205 206 204
-
-Sample Output
-204 205 206
-
-  
-  // Solution 
+// Solution 
 
   
 public class Solution {
