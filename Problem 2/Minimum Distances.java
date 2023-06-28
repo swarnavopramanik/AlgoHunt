4e7b249dce67174b31091dd49c5550fbf0e4d5ee
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-28 20:30:06 +0530

    Create Minimum Distances.java

---
+++ Problem 2/Minimum Distances.java
@@ -0,0 +1,62 @@
+
+The distance between two array values is the number of indices between them. Given , 
+  
+find the minimum distance between any pair of equal elements in the array. If no such value exists, return .
+
+Example
+
+There are two matching pairs of values:  and . The indices of the 's are  and , so their distance is . 
+The indices of the 's are  and , so their distance is . The minimum distance is .
+
+Function Description
+
+Complete the minimumDistances function in the editor below.
+
+minimumDistances has the following parameter(s):
+
+int a[n]: an array of integers
+Returns
+
+int: the minimum distance found or  if there are no matching elements
+Input Format
+
+The first line contains an integer , the size of array .
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print a single integer denoting the minimum  in . If no such value exists, print .
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6               arr[] size n = 6
+7 1 3 4 1 7     arr = [7, 1, 3, 4, 1, 7]
+Sample Output
+3
+
+  // Solution 
+
+  public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
