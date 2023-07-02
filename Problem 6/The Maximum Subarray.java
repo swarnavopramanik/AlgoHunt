a4c2496dfe7b922d097d16ea822961d6de0e3795
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-07-02 10:18:25 +0530

    Update The Maximum Subarray.java

--- Problem 6/The Maximum Subarray.java
+++ Problem 6/The Maximum Subarray.java
@@ -4,36 +4,53 @@ Question Link 👇
 
 // Solution 
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int t = Integer.parseInt(bufferedReader.readLine().trim());
-
-        IntStream.range(0, t).forEach(tItr -> {
-            try {
-                int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                    .map(Integer::parseInt)
-                    .collect(toList());
-
-                List<Integer> result = Result.maxSubarray(arr);
-
-                bufferedWriter.write(
-                    result.stream()
-                        .map(Object::toString)
-                        .collect(joining(" "))
-                    + "\n"
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+        int T = myScan.nextInt();
+        while(T--!=0){
+            int N = myScan.nextInt();
+            int[] arr = new int[N];
+            for(int i=0; i<N; i++){
+                arr[i]=myScan.nextInt();
             }
-        });
-
-        bufferedReader.close();
-        bufferedWriter.close();
+            int curr_sum=0;
+            int best_sum=0;
+            int non_cont_sum=0;
+            for(int j=0; j<N; j++){
+                curr_sum += arr[j];
+                if(curr_sum>0){
+                    if(curr_sum>best_sum){
+                        best_sum=curr_sum;
+                    }
+                }else{
+                    curr_sum=0;
+                }
+                if(arr[j]>0){
+                    non_cont_sum +=arr[j];
+                }
+            }
+            if(best_sum ==0 && non_cont_sum==0){
+                int max = arr[0];
+                for(int l=1; l<N; l++){
+                    if(arr[l]>max){
+                        max=arr[l];
+                    }
+                }
+                best_sum=max;
+                non_cont_sum=max;
+            }
+            System.out.print(best_sum+" "+non_cont_sum);
+            System.out.println();
+        }
     }
 }
+
+
